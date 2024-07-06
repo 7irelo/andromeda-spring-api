43f184cd6990f713fb88dcd10c46b0faf28c02c0
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-06 17:48:53 +0200

    Create Like.java

---
+++ andromeda/src/main/java/com/andromeda/model/Like.java
@@ -0,0 +1,21 @@
+package com.example.andromeda.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", referencedColumnName = "id")
+    private Post post;
+
+    // Getters and Setters
+}
