37ab60af91a9340abc4d02029fa184597ccd414d
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-06 14:46:11 +0200

    Create UserService.java

---
+++ andromeda/src/main/java/com/andromeda/services/UserService.java
@@ -0,0 +1,31 @@
+package com.example.andromeda.service;
+
+import com.example.andromeda.model.User;
+import com.example.andromeda.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
