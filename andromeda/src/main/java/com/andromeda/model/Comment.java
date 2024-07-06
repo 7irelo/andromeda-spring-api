80834f1b6d970109bd9ae39e68f609fcd5c88926
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-06 17:49:33 +0200

    Create Comment.java

---
+++ andromeda/src/main/java/com/andromeda/model/Comment.java
@@ -0,0 +1,26 @@
+package com.example.andromeda.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private Date createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", referencedColumnName = "id")
+    private Post post;
+
+    // Getters and Setters
+}
