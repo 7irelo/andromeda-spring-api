1397ab917aa83fe76831a071349dfd4207d16370
7irelo <155379151+7irelo@users.noreply.github.com>
2024-07-06 17:59:02 +0200

    Create NotificationRepository.java

---
+++ andromeda/src/main/java/com/andromeda/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.example.andromeda.repository;
+
+import com.example.andromeda.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserId(Long userId);
+}
